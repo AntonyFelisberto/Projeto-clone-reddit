b3e30107dd7d185e30b228a78537f515102109b7
Antony dos santos felisberto <antony.dos.santos.felisberto@gmail.com>
2022-09-28 20:47:09 -0300

    tentando corrigir erro: Parameter 0 of constructor in com.example.reddit.service.MailContentBuilder required a bean of type 'org.thymeleaf.TemplateEngine' that could not be found.
    Ajuste na classe Vote

--- redditBackEnd/src/main/java/com/example/reddit/model/Vote.java
+++ redditBackEnd/src/main/java/com/example/reddit/model/Vote.java
@@ -25,7 +25,7 @@ public class Vote {
     @NotNull
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "postId", referencedColumnName = "postId")
-    private Post postId;
+    private Post post;
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "userId", referencedColumnName = "userId")
     private User userId;
